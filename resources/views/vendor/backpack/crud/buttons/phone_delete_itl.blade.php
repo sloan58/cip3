ff8b8e593e33872d8cc3040f7ce7412b5d7690ed
sloan58 <martin.sloan@karma-tek.com>
2019-08-14 10:47:55 -0400

    don't show actions for phones that aren't registered

--- resources/views/vendor/backpack/crud/buttons/phone_delete_itl.blade.php
+++ resources/views/vendor/backpack/crud/buttons/phone_delete_itl.blade.php
@@ -1,4 +1,4 @@
-@if ($entry->itl)
+@if ($entry->itl && $entry->isRegistered())
     <a href="{{ url($crud->route.'/'.$entry->getKey().'/delete-itl') }}" class="btn btn-xs btn-default" style="color:green">
         <i class="fa fa-bomb"></i> Delete ITL
     </a>
