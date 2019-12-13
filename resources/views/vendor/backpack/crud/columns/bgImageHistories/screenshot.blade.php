ab05162ef0b5a4512d3208dfda46cedb686b2fa4
sloan58 <martin.sloan@karma-tek.com>
2019-12-12 19:36:55 -0500

    fix bug in screen shot url

--- resources/views/vendor/backpack/crud/columns/bgImageHistories/screenshot.blade.php
+++ resources/views/vendor/backpack/crud/columns/bgImageHistories/screenshot.blade.php
@@ -1,6 +1,6 @@
 @if($entry->result == "success")
-<a href="https://cip3.test/storage/screenshots/{{$entry->id}}_{{$entry->phone}}.png" target="_blank">
-    <img src="https://cip3.test/storage/screenshots/{{$entry->id}}_{{$entry->phone}}.png" style="
+<a href="{{env('APP_URL')}}/storage/screenshots/{{$entry->id}}_{{$entry->phone}}.png" target="_blank">
+    <img src="{{env('APP_URL')}}/storage/screenshots/{{$entry->id}}_{{$entry->phone}}.png" style="
         height: 106px;
         width: 160px;
         border-radius: 3px;">
